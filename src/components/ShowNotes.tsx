b6c1240b174517ff0154a75dbb22b27bcdd1def5
Erik Rasmussen <rasmussenerik@gmail.com>
2019-04-01 12:41:32 +0200

    Styled blockquotes in shownotes

--- src/components/ShowNotes.tsx
+++ src/components/ShowNotes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, Paragraph } from 'grommet'
 import { Episode } from '../types'
+import styled from 'styled-components'
 import { withRouteData } from 'react-static'
 
 type Props = { content?: Episode; episode?: Episode }
@@ -8,13 +9,20 @@ export default withRouteData(({ content, episode }: Props) => {
   const ep = episode || content
   if (!ep || !ep.body) return 'no content'
   return (
-    <Box fill>
+    <Container fill>
       <Paragraph
         key={ep.frontmatter.slug}
         style={{ maxWidth: '100%', width: '100%' }}
       >
         <div dangerouslySetInnerHTML={{ __html: ep.body }} />
       </Paragraph>
-    </Box>
+    </Container>
   )
 })
+
+const Container = styled(Box)`
+  blockquote {
+    padding-left: 20px;
+    border-left: 1px solid #ccc;
+  }
+`
