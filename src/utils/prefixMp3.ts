14fa35a2a0c9eea602c49bbfec51f61a115eeda0
Erik Rasmussen <rasmussenerik@gmail.com>
2019-05-21 23:01:44 +0200

    Enabled chartable

--- src/utils/prefixMp3.ts
+++ src/utils/prefixMp3.ts
@@ -1,6 +1,6 @@
 const prefixes = [
   'https://dts.podtrac.com/redirect.mp3/',
-  // 'https://chtbl.com/track/73173/',
+  'https://chtbl.com/track/9DG54/',
 ]
 
 export const prefixMp3 = (file: string) =>
