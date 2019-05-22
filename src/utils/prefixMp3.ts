1fb3ca712a74e72db9baba6fb8c25425f18a839a
Erik Rasmussen <rasmussenerik@gmail.com>
2019-05-22 10:05:49 +0200

    Fixed url for mp3s

--- src/utils/prefixMp3.ts
+++ src/utils/prefixMp3.ts
@@ -3,5 +3,5 @@ const prefixes = ['dts.podtrac.com/redirect.mp3/', 'chtbl.com/track/9DG54/']
 export const prefixMp3 = (file: string) =>
   `https://${prefixes.reduce(
     (result, prefix) => `${prefix}${result}`,
-    `happyhour.fm/media/${file}`,
+    `seekjustice.fm/media/${file}`,
   )}`
